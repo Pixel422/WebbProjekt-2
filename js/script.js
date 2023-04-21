d8b48c22a17bb19fa02197c2ab25a3b70b996370
Pixel422 <abben1000@gmail.com>
2023-04-21 12:05:24 +0200

    progress

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//Knappen för att scrolla ner
 const scrollContainer = document.querySelector('.scrollContainer');
   const scroller = document.querySelector('.scroller');
   
@@ -7,3 +8,20 @@ const scrollContainer = document.querySelector('.scrollContainer');
       behavior: 'smooth'});
 
   });
+
+  //Betygsättning
+  const stars = document.querySelectorAll('.star');
+
+stars.forEach((star, index) => {
+  star.addEventListener('mouseover', () => {
+    for (let i = 0; i < index; i++) {
+      stars[i].style.filter = 'invert(76%) sepia(23%) saturate(2144%) hue-rotate(0deg) brightness(105%) contrast(105%)';
+    }
+  });
+
+  star.addEventListener('mouseout', () => {
+    for (let i = 0; i < index; i++) {
+      stars[i].style.filter = '';
+    }
+  });
+});
